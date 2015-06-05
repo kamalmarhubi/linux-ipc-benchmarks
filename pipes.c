d46248553a8f9ab8688545b7121b74d87cb577c5
Kamal Marhubi <kamal@marhubi.com>
2015-06-05 15:20:12 -0400

    Take number of iterations from the command line

--- pipes.c
+++ pipes.c
@@ -1,19 +1,54 @@
+#include <argp.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
-#define DEFAULT_ITERS 100000
-#define WARMUP_ITERS 10000
+#define xstr(s) str(s)
+#define str(s) #s
 
-void parent_loop(long iters, int tx_fd, int rx_fd) {
+#define DEFAULT_ITERS 100000
+#define DEFAULT_WARMUP_ITERS 1000
+
+static struct argp_option options[] = {
+    {"iters", 'i', "COUNT", 0, "number of iterations to measure; default: " xstr(DEFAULT_ITERS), 0},
+    {0, 'n', "COUNT", OPTION_ALIAS, 0, 0},
+    {"warmup-iters", 'w', "COUNT", 0, "number of iterations before measurement; default: " xstr(DEFAULT_WARMUP_ITERS), 0},
+    {0}
+};
+
+typedef struct {
+    int iters;
+    int warmup_iters;
+} args;
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    args* args = state->input;
+
+    switch(key) {
+        case 'i':
+        case 'n':
+            args->iters = atoi(arg);
+            break;
+        case 'w':
+            args->warmup_iters = atoi(arg);
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, 0, 0, 0, 0, 0};
+
+void parent_loop(int warmup_iters, int iters, int tx_fd, int rx_fd) {
     int i;
     struct timespec start, end;
 
     fprintf(stderr, "About to start!\n");
 
-    for (i = 0; i < WARMUP_ITERS; ++i) {
+    for (i = 0; i < warmup_iters; ++i) {
         char resp;
         if (write(tx_fd, "0", 1) == -1) {
             perror("could not write");
@@ -48,7 +83,7 @@ void parent_loop(long iters, int tx_fd, int rx_fd) {
 
     long long elapsed_nsec = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
 
-    fprintf(stderr, "%ld iters in %lld ns\n %f ns/iter\n", iters, elapsed_nsec, (double) elapsed_nsec / iters);
+    fprintf(stderr, "%d iters in %lld ns\n %f ns/iter\n", iters, elapsed_nsec, (double) elapsed_nsec / iters);
 
     exit(EXIT_SUCCESS);
 }
@@ -62,20 +97,12 @@ void child_loop(int tx_fd, int rx_fd) {
 }
 
 int main(int argc, char **argv) {
-    long iters;
+    args args;
 
-    if (argc > 1) {
-        errno = 0;
-        iters = strtol(argv[1], NULL, 10);
-        if (errno) {
-            perror("if provided, first argument must be an integer");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        iters = DEFAULT_ITERS;
-    }
+    args.iters = DEFAULT_ITERS;
+    args.warmup_iters = DEFAULT_WARMUP_ITERS;
 
-    fprintf(stderr, "iters: %ld\n", iters);
+    argp_parse(&argp, argc, argv, 0, 0, &args);
 
     int tx[2], rx[2];  // relative to parent
     if (pipe(tx)) {
@@ -93,7 +120,7 @@ int main(int argc, char **argv) {
         close(tx[0]);
         close(rx[1]);
 
-        parent_loop(iters, tx[1], rx[0]);
+        parent_loop(args.warmup_iters, args.iters, tx[1], rx[0]);
     } else {
         close(tx[1]);
         close(rx[0]);
