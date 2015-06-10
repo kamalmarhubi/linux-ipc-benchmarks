854b02bb5a2f830f03a754bc1b16fcd6df0704a7
Kamal Marhubi <kamal@marhubi.com>
2015-06-10 11:03:36 -0400

    Allow pinning the processes to specific CPUs
    The latency is affected by whether the processes are on the same CPU or
    not, so allow setting this from the command line.

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
+/* _GNU_SOURCE is required for CPU_SET macros in sched.h */
+#define _GNU_SOURCE
+
 #include <argp.h>
+#include <sched.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,11 +14,18 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
+#define DEFAULT_CPU -1
 #define DEFAULT_MEASUREMENTS 1
 #define DEFAULT_ITERS 100000
 #define DEFAULT_WARMUP_ITERS 1000
 
 static struct argp_option options[] = {
+    {"child-cpu", 'c', "CPUID", 0,
+     "CPU to run the child on; default is to let the scheduler do as it will",
+     0},
+    {"parent-cpu", 'p', "CPUID", 0,
+     "CPU to run the parent on; default is to let the scheduler do as it will",
+     0},
     {"repeat", 'r', "COUNT", 0,
      "number of times to repeat measurement; default: " xstr(DEFAULT_MEASUREMENTS), 0},
     {"iters", 'i', "COUNT", 0,
@@ -27,6 +38,8 @@ static struct argp_option options[] = {
     {0}};
 
 typedef struct {
+  int child_cpu;
+  int parent_cpu;
   int iters;
   int repeats;
   int warmup_iters;
@@ -36,6 +49,12 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
   args *args = state->input;
 
   switch (key) {
+    case 'c':
+      args->child_cpu = atoi(arg);
+      break;
+    case 'p':
+      args->parent_cpu = atoi(arg);
+      break;
     case 'r':
       args->repeats = atoi(arg);
       break;
@@ -59,10 +78,27 @@ long long elapsed_nsec(struct timespec start, struct timespec end) {
 
 static struct argp argp = {options, parse_opt, 0, 0, 0, 0, 0};
 
+void sched_setaffinity_or_die(int cpu_id) {
+  if (cpu_id == DEFAULT_CPU) {
+    return;
+  }
+
+  cpu_set_t cpu_set;
+  CPU_ZERO(&cpu_set);
+  CPU_SET(cpu_id, &cpu_set);
+
+  if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &cpu_set) == -1) {
+    perror("could not set CPU affinity");
+    exit(EXIT_FAILURE);
+  }
+}
+
 int main(int argc, char **argv) {
   args args;
   state *state = new_state();
 
+  args.child_cpu = DEFAULT_CPU;
+  args.parent_cpu = DEFAULT_CPU;
   args.iters = DEFAULT_ITERS;
   args.repeats = DEFAULT_MEASUREMENTS;
   args.warmup_iters = DEFAULT_WARMUP_ITERS;
@@ -76,6 +112,7 @@ int main(int argc, char **argv) {
     perror("could not fork");
     exit(EXIT_FAILURE);
   } else if (child) {
+    sched_setaffinity_or_die(args.parent_cpu);
     parent_post_fork_setup(state);
     parent_warmup(args.warmup_iters, state);
 
@@ -101,6 +138,7 @@ int main(int argc, char **argv) {
     kill(child, SIGTERM);
 
   } else {
+    sched_setaffinity_or_die(args.child_cpu);
     child_post_fork_setup(state);
     child_warmup(args.warmup_iters, state);
     child_loop(args.iters, state);
